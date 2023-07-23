67433adbf0edea9fa3b4aec3e1f9f5c1c50b81bb
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2023-07-23 19:28:15 +0300

    clean

--- main.go
+++ main.go
@@ -11,14 +11,8 @@ func main() {
 	fs := http.FileServer(http.Dir(iconsDir))
 
 	http.Handle("/icons/", http.StripPrefix("/icons/", fs))
-	http.HandleFunc("/icons/centria", serveIconsBreeze)
-	http.HandleFunc("/icons/centriaDark", serveIconsCentriaDark)
-	http.HandleFunc("/icons/breeze", serveIconsBreeze)
-	http.HandleFunc("/icons/oxygen", serveIconsOxygen)
-	http.HandleFunc("/post/centria", postIconsCentria)
-	http.HandleFunc("/post/centriaDark", postIconsCentriaDark)
-	http.HandleFunc("/post/breeze", postIconsBreeze)
-	http.HandleFunc("/post/oxygen", postIconsOxygen)
+	http.HandleFunc("/get/", serveIcons)
+	http.HandleFunc("/post/centria", postIcons)
 
 	fmt.Println("serving at port: 8080")
 	err := http.ListenAndServe(":8080", nil)
--- wetCode.go
+++ wetCode.go
@@ -3,48 +3,26 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+	"strings"
 )
 
-func getIconsCentria(w http.ResponseWriter, r *http.Request) {
-	icons := getIcons("./icons/centria/", "")
+func serveIcons(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	err := json.NewEncoder(w).Encode(icons)
-	if err != nil {
-		http.Error(w, "Couldn't encode JSON", http.StatusInternalServerError)
-		return
+	path := r.URL.Path
+	segments := strings.Split(path, "/")
+
+	var selectedValue string
+	for i, seg := range segments {
+		if seg == "get" && i+1 < len(segments) {
+			selectedValue = segments[i+1]
+			break
+		}
 	}
-}
-func serveIconsCentriaDark(w http.ResponseWriter, r *http.Request) {
-	icons := getIcons("./icons/centria-dark/", "")
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	err := json.NewEncoder(w).Encode(icons)
-	if err != nil {
-		http.Error(w, "Couldn't encode JSON", http.StatusInternalServerError)
-		return
-	}
-}
-func serveIconsBreeze(w http.ResponseWriter, r *http.Request) {
-	icons := getIcons("./icons/breeze/", "")
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	err := json.NewEncoder(w).Encode(icons)
-	if err != nil {
-		http.Error(w, "Couldn't encode JSON", http.StatusInternalServerError)
-		return
-	}
-}
-func serveIconsOxygen(w http.ResponseWriter, r *http.Request) {
-	icons := getIcons("./icons/oxygen/", "")
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
+	icons := getIcons(fmt.Sprintf("./icons/%s/", selectedValue), "")
 	err := json.NewEncoder(w).Encode(icons)
 	if err != nil {
 		http.Error(w, "Couldn't encode JSON", http.StatusInternalServerError)
@@ -58,14 +36,13 @@ type IconData struct {
 }
 
 // POSTS
-func postIconsCentria(w http.ResponseWriter, r *http.Request) {
+func postIcons(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	// Parse the JSON payload from the request body
 	var data IconData
-	fmt.Printf(data.Theme)
 	err := json.NewDecoder(r.Body).Decode(&data)
 	icons := getIcons(fmt.Sprintf("./icons/%s/", data.Theme), "")
 	var newArray []Icon
@@ -77,78 +54,7 @@ func postIconsCentria(w http.ResponseWriter, r *http.Request) {
 			newArray = append(newArray, icon)
 		}
 	}
-	// You can access the received data now
-
-	fmt.Printf("Icons: %v\n", data.Icons)
 	// Sending the response back to the client as JSON
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(newArray)
-}
-func postIconsCentriaDark(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-		return
-	}
-	icons := getIcons("./icons/centria-dark/", "")
-	neededIcons := parseItems(string(body))
-	var newArray []Icon
-	for _, icon := range icons {
-		if containsString(neededIcons, icon.Name) {
-			newArray = append(newArray, icon)
-		}
-	}
-
-	// Send the response back to the client
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(newArray)
-}
-func postIconsBreeze(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("connection ...")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-		return
-	}
-	icons := getIcons("./icons/breeze/", "")
-	neededIcons := parseItems(string(body))
-
-	var newArray []Icon
-	for _, icon := range icons {
-		if containsString(neededIcons, icon.Name) {
-			newArray = append(newArray, icon)
-		}
-	}
-	fmt.Println("sending....")
-	// Send the response back to the client
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(newArray)
-}
-func postIconsOxygen(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-		return
-	}
-	icons := getIcons("./icons/oxygen/", "")
-	neededIcons := parseItems(string(body))
-	var newArray []Icon
-	for _, icon := range icons {
-		if containsString(neededIcons, icon.Name) {
-			newArray = append(newArray, icon)
-		}
-	}
-
-	// Send the response back to the client
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(newArray)
 }
